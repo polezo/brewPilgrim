c46bc1401a304736fd134b716b33818a11f31387
Adam Finch <atyreefinch@gmail.com>
2019-10-21 16:43:31 -0400

    add google maps to homepage search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,22 +1,12 @@
 class SearchesController < ApplicationController
 
     def home
-        if !params.empty?
-            search_term = params[:search]
-            response = RestClient.get "https://api.openbrewerydb.org/breweries?by_city=#{search_term}"
-        
-            json = JSON.parse response
-        
-            if !json.length.zero?
-                @breweries = json.select { 
-                    |brewery| brewery if brewery['brewery_type'] != 'planning'
-                }
-            else
+        if  params[:search]
+            search_city = params[:search].titleize
+            @breweries = Brewery.where(city: search_city)
+            if @breweries.length.zero?
                 render :home
             end
         end
     end
-
-
-
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,41 @@
 class Brewery < ApplicationRecord
     has_many :brewqueues
+
+    def maps_address
+        if self.city && self.state
+            if self.name.include?(" ") 
+                 name = self.name.split(" ").join("+") 
+            else
+                 name = self.name
+            end
+            
+            if self.city.include?(" ") 
+                city = self.city.split(" ").join("+") 
+            else
+                city = self.city
+            end
+            
+            if self.state.include?(" ") 
+                state = self.state.split(" ").join("+") 
+            else
+                state = self.state
+            end
+
+            if self.street.empty?
+              address = name + "+" + city + "+" + state
+            else 
+                street = self.street.split(" ").join("+")
+                address = name + "+" + street + "+" + city + "+" + state
+            end
+        else 
+            return false
+        end
+        return "https://www.google.com/maps/search/?api=1&query=#{address}"
+    end
+
+
+    def addy
+        self.street + " " + self.zip_code
+    end
+
 end
