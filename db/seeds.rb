91b05d2b24b5a46bf4d344dae8778cc63924edd0
Adam Finch <atyreefinch@gmail.com>
2019-10-21 14:43:05 -0400

    seeded with breweries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+i = 1
+breweries = []
+
+161.times do
+response = RestClient.get "https://api.openbrewerydb.org/breweries?page=#{i}&per_page=50"
+json = JSON.parse response
+
+breweries << json
+i += 1
+end
+
+flattened_breweries = breweries.flatten
+
+flattened_breweries.each do |brewery|
+    if brewery['brewery_type'] != 'planning' 
+        Brewery.create(
+            name: brewery["name"], 
+            brewery_type: brewery["brewery_type"], 
+            street: brewery["street"],
+            city: brewery["city"],
+            state: brewery["state"], 
+            zip_code: brewery["postal_code"],
+            country: brewery["country"],
+            latitude: brewery["latitude"], 
+            longitude: brewery["longitude"], 
+            phone: brewery["phone"],
+            website_url: brewery["website_url"])
+
+        if brewery["tag_list"].length > 0 
+            brewery["tag_list"].each do |tag|
+                Tag.find_or_create_by(name: tag)
+            end
+        end
+    end
+end
+
+
