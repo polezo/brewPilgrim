8354af49bdeb0aae790cd9fbdf9049f8ef12771d
nihaprezz <sergio13prez@gmail.com>
2019-10-21 17:35:06 -0400

    finished user and pw logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :bio, presence: true
+
     has_secure_password
+
+
 end
