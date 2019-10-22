5b255369bb508af16316d470cb5ca42ef78888aa
Adam Finch <atyreefinch@gmail.com>
2019-10-22 12:24:43 -0400

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 #when logged in will give the user their show page
-get '/profile/:id', to: 'users#profile', as: 'profile'
+get '/profile', to: 'users#profile', as: 'profile'
 
 get '/', to: 'searches#home', as: 'home'
 # get '/results:q', to: 'searches#results'
