3b28a0b7d1ccd870c61119e3d59df08aa6a41081
Adam Finch <atyreefinch@gmail.com>
2019-10-23 15:31:38 -0400

    stars css and starting brequeue destroy

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,34 @@ class Review < ApplicationRecord
     belongs_to :reviewer, class_name: "User", foreign_key: :reviewer_id
     belongs_to :reviewee, class_name: "Brewery", foreign_key: :reviewee_id
 
+    def stars
+        if self.rating == 1
+            starz = "★"
+        elsif self.rating == 2
+            starz = "★★"
+        elsif self.rating == 3
+            starz = "★★★"
+        elsif self.rating == 4
+            starz = "★★★★"
+        elsif self.rating == 5
+            starz = "★★★★★"
+        end  
+        starz  
+    end
+
+    def grey_stars
+        if self.rating == 4
+            stary = "★"
+        elsif self.rating == 3
+            stary = "★★"
+        elsif self.rating == 2
+            stary = "★★★"
+        elsif self.rating == 1
+            stary = "★★★★"
+        elsif self.rating == 5
+            stary = ""
+        end 
+        stary  
+    end
+
 end
