e1e743b2273d0fc31ffe6aceb55fba79a1b8945c
Adam Finch <atyreefinch@gmail.com>
2019-10-24 09:39:23 -0400

    brewqueue homepage in html

--- app/controllers/brewqueues_controller.rb
+++ app/controllers/brewqueues_controller.rb
@@ -1,16 +1,19 @@
 class BrewqueuesController < ApplicationController
 
     def create
-        if cookies[:last_visited_home]
-            params[:brewqueue].each do |bq|
+        if cookies[:last_visited] == "home"
+            params[:brewqueues].each do |bq|
                 new_bq = Brewqueue.new(brewery_id:bq,user:current_user)
+                byebug
                 new_bq.save
             end
+            redirect_to profile_path(current_user)
         else
             brewq = Brewqueue.new(brewqueue_params)
             brewq.save
+            redirect_to brewery_path(brewq.brewery)
         end
-        redirect_to profile_path(current_user)
+        
     end
 
     def destroy
